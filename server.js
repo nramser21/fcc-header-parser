d3fdd39f690e7a5b37f1f76cd2ab685b6fc6f507
Nick <nramser21@gmail.com>
2017-06-02 22:28:48 -0400

    Sends ip adress

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 var express = require('express');
 var app = express();
+
+app.get('/', (req, res) => {
+  console.log(req.headers);
+  res.send(req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress);
+});
+
+app.listen(process.env.PORT || 3000)
