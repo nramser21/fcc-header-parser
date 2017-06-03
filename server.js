82f4c9a772608c277eb32e257a99dc095323b5ce
Nick <nramser21@gmail.com>
2017-06-02 22:51:09 -0400

    Respond to request with ip, lang, and os

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  console.log(req.headers);
-  res.send(req.headers['x-forwarded-for'] ||
-     req.connection.remoteAddress ||
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress);
+  var info = {
+    ipaddress: null,
+    language: null,
+    software: null
+  }
+
+  info.ipaddress = req.headers['x-forwarded-for'] ||
+           req.connection.remoteAddress ||
+           req.socket.remoteAddress ||
+           req.connection.socket.remoteAddress;
+
+  info.software = req.headers['user-agent'].split(')')[0].split('(')[1];
+  info.language = req.headers['accept-language'].split(',')[0];
+  res.send(info);
 });
 
 app.listen(process.env.PORT || 3000)
